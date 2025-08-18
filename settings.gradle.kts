f31c794c1004b88d912521ef02a157f641f8ef15
KingBrutblarg <cheflauro316@gmail.com>
2025-08-18 05:28:30 -0600

    Sync: ajustes locales Gradle

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,13 +1,24 @@
+// settings.gradle.kts
 pluginManagement {
-  repositories { google(); mavenCentral(); gradlePluginPortal() }
+  repositories {
+    google()
+    mavenCentral()
+    gradlePluginPortal()
+  }
   plugins {
-    id("com.android.application") version "8.5.2"
+    id("com.android.application") version "8.4.2"
+    id("com.android.library")     version "8.4.2"
     id("org.jetbrains.kotlin.android") version "1.9.24"
   }
 }
+
 dependencyResolutionManagement {
   repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
-  repositories { google(); mavenCentral() }
+  repositories {
+    google()
+    mavenCentral()
+  }
 }
+
 rootProject.name = "FreyjaBeta"
 include(":app")
