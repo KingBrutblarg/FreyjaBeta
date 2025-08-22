6636324e9871fad041f36d07b2a00f47c1989b61
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-22 11:39:08 -0600

    Actualizar build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,48 +1,49 @@
 plugins {
-    id 'com.android.application'
-    id 'org.jetbrains.kotlin.android'
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
 }
 
 android {
-    namespace 'com.angeluz.freyja'
-    compileSdk 34
+    namespace = "com.angeluz.freyja"
+    compileSdk = 34
 
     defaultConfig {
-        applicationId "com.angeluz.freyja"
-        minSdk 26
-        targetSdk 34
-        versionCode 1
-        versionName "1.0"
+        applicationId = "com.angeluz.freyja"
+        minSdk = 26
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
     }
 
     buildTypes {
         release {
-            minifyEnabled false
-            proguardFiles getDefaultProguardFile('proguard-android-optimize.txt'), 'proguard-rules.pro'
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
         }
         debug {
-            minifyEnabled false
+            isMinifyEnabled = false
         }
     }
 
     compileOptions {
-        sourceCompatibility JavaVersion.VERSION_17
-        targetCompatibility JavaVersion.VERSION_17
+        sourceCompatibility = JavaVersion.VERSION_17
+        targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
-        jvmTarget = '17'
+        jvmTarget = "17"
     }
 }
 
 dependencies {
-    implementation "androidx.core:core-ktx:1.13.1"
-    implementation "androidx.appcompat:appcompat:1.7.0"
-    implementation "com.google.android.material:material:1.12.0"
-    implementation "androidx.activity:activity-ktx:1.9.2"
-    implementation "androidx.lifecycle:lifecycle-runtime-ktx:2.8.4"
-    implementation "androidx.work:work-runtime-ktx:2.9.1"
-    implementation "androidx.core:core-splashscreen:1.0.1"
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
 
-    // Notificaciones
-    implementation "androidx.core:core-ktx:1.13.1"
+    implementation("androidx.activity:activity-ktx:1.9.2")
+    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.4")
+    implementation("androidx.work:work-runtime-ktx:2.9.1")
+    implementation("androidx.core:core-splashscreen:1.0.1")
 }
