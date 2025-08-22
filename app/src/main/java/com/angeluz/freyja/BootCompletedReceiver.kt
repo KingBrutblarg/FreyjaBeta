eff2c83933e3aa6d8e39c9a679bdfb2b6257117d
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-22 11:33:06 -0600

    Actualizar BootCompletedReceiver.kt

--- app/src/main/java/com/angeluz/freyja/BootCompletedReceiver.kt
+++ app/src/main/java/com/angeluz/freyja/BootCompletedReceiver.kt
@@ -1,11 +1,15 @@
 package com.angeluz.freyja
+
 import android.content.BroadcastReceiver
 import android.content.Context
 import android.content.Intent
+import androidx.core.content.ContextCompat
+
 class BootCompletedReceiver : BroadcastReceiver() {
-  override fun onReceive(context: Context, intent: Intent) {
-    if (Intent.ACTION_BOOT_COMPLETED == intent.action) {
-      context.startForegroundService(Intent(context, FreyjaService::class.java))
+    override fun onReceive(context: Context, intent: Intent) {
+        // Arranca el servicio tras reinicio
+        ContextCompat.startForegroundService(
+            context, Intent(context, FreyjaService::class.java)
+        )
     }
-  }
 }
