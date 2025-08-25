ad6d42ac2bfa9aa226eb97639308628c7cd6f5ba
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-24 21:18:58 -0600

    Crear ChatApi.kt

---
+++ app/src/main/java/com/angeluz/freyja/data/ChatApi.kt
@@ -0,0 +1,27 @@
+package com.angeluz.freyja.data
+
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import retrofit2.http.Body
+import retrofit2.http.POST
+
+// --- Request y Reply (puedes ajustarlos según tu backend)
+data class ChatRequest(val prompt: String)
+data class ChatReply(val text: String)
+
+// --- API REST
+interface ChatApi {
+    @POST("chat")
+    suspend fun chat(@Body req: ChatRequest): ChatReply
+}
+
+// --- Retrofit Provider
+object RetrofitProvider {
+    val api: ChatApi by lazy {
+        Retrofit.Builder()
+            .baseUrl("https://TU_BACKEND/") // TODO: cámbialo por tu endpoint real
+            .addConverterFactory(MoshiConverterFactory.create())
+            .build()
+            .create(ChatApi::class.java)
+    }
+}
