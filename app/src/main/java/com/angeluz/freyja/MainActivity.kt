9e1686fcf9d1df263d1d0f8a05248adfe165a495
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-25 10:05:41 -0600

    Actualizar MainActivity.kt

--- app/src/main/java/com/angeluz/freyja/MainActivity.kt
+++ app/src/main/java/com/angeluz/freyja/MainActivity.kt
@@ -1,21 +1,17 @@
 package com.angeluz.freyja
-import com.angeluz.freyja.ChatViewModel
 
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
-import com.angeluz.freyja.ui.screens.FreyjaScreen
+import com.angeluz.freyja.ui.screens.ChatScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-        // Evita trabajo pesado en UI aquí; inicializa perezoso
         setContent {
             MaterialTheme {
-                FreyjaScreen(vm = ChatViewModel())
-                        com.angeluz.freyja.ui.screens.ImageGenScreen()
+                ChatScreen()
             }
         }
     }
