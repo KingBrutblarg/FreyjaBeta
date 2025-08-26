867c064b2c4a020be138a1216d09f8c62b13235d
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-26 12:47:48 -0600

    Actualizar build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,20 @@
-plugins {
-    id("com.android.application") version "8.4.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.25" apply false
+android {
+    signingConfigs {
+        create("release") {
+            storeFile = file("freyja-release.keystore")
+            storePassword = System.getenv("ANDROID_KEYSTORE_PASSWORD")
+            keyAlias = System.getenv("ANDROID_KEY_ALIAS")
+            keyPassword = System.getenv("ANDROID_KEY_PASSWORD")
+        }
+    }
+    buildTypes {
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+            isMinifyEnabled = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
 }
