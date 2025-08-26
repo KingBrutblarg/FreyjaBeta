2f6af811567208bb02ae3a4ae7905adb79e20f11
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-25 20:35:09 -0600

    Actualizar RetrofitProvider.kt

--- app/src/main/java/com/angeluz/freyja/data/RetrofitProvider.kt
+++ app/src/main/java/com/angeluz/freyja/data/RetrofitProvider.kt
@@ -1,33 +1,20 @@
 package com.angeluz.freyja.data
 
 import com.angeluz.freyja.BuildConfig
-import com.squareup.moshi.Moshi
-import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
 
 object RetrofitProvider {
-
-    private val moshi: Moshi by lazy {
-        Moshi.Builder()
-            .add(KotlinJsonAdapterFactory())
-            .build()
+    private val okHttp by lazy {
+        OkHttpClient.Builder().build()
     }
 
-    private val client: OkHttpClient by lazy {
-        OkHttpClient.Builder()
-            // .addInterceptor(...) // si necesitas auth/logging
-            .build()
-    }
-
-    private val retrofit: Retrofit by lazy {
+    val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BuildConfig.API_BASE_URL) // definido en build.gradle.kts
-            .addConverterFactory(MoshiConverterFactory.create(moshi))
-            .client(client)
+            .baseUrl(BuildConfig.API_BASE_URL) // termina en "/"
+            .client(okHttp)
+            .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
-
-    val api: ChatApi by lazy { retrofit.create(ChatApi::class.java) }
 }
