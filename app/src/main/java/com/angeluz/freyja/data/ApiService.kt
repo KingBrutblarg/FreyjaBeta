5e8109ee94427a3e03c02f3bf33af81b22dff304
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-27 09:33:10 -0600

    Actualizar ApiService.kt

--- app/src/main/java/com/angeluz/freyja/data/ApiService.kt
+++ app/src/main/java/com/angeluz/freyja/data/ApiService.kt
@@ -2,13 +2,14 @@ package com.angeluz.freyja.data
 
 import retrofit2.http.GET
 
+// Define tu API (ajusta el endpoint según tu backend)
 interface ApiService {
     @GET("posts")
     suspend fun getPosts(): List<PostDto>
 }
 
+// DTO temporal
 data class PostDto(
-    val userId: Int,
     val id: Int,
     val title: String,
     val body: String
