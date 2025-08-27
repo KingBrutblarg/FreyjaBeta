ea9d8c6cd306f197cc595cf7de96ef1e4629361c
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-08-27 06:38:51 -0600

    Actualizar RetrofitProvider.kt

--- app/src/main/java/com/angeluz/freyja/data/RetrofitProvider.kt
+++ app/src/main/java/com/angeluz/freyja/data/RetrofitProvider.kt
@@ -4,16 +4,26 @@ import com.angeluz.freyja.BuildConfig
 import okhttp3.OkHttpClient
 import retrofit2.Retrofit
 import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
+/**
+ * Proveedor singleton de Retrofit.
+ * Usa BuildConfig.API_BASE_URL (definido en build.gradle.kts).
+ */
 object RetrofitProvider {
-    private val okHttp by lazy {
-        OkHttpClient.Builder().build()
+
+    private val client: OkHttpClient by lazy {
+        OkHttpClient.Builder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .build()
     }
 
     val retrofit: Retrofit by lazy {
         Retrofit.Builder()
-            .baseUrl(BuildConfig.API_BASE_URL) // termina en "/"
-            .client(okHttp)
+            .baseUrl(BuildConfig.API_BASE_URL) // <<--- Aquí se inyecta tu endpoint
+            .client(client)
             .addConverterFactory(MoshiConverterFactory.create())
             .build()
     }
