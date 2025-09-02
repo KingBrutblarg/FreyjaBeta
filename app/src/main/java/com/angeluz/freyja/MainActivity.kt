e2ec6d2c78420ed2c35e74b69eb866c19126c927
KingBrutblarg <75027990+KingBrutblarg@users.noreply.github.com>
2025-09-02 13:02:16 -0600

    Actualizar MainActivity.kt

--- app/src/main/java/com/angeluz/freyja/MainActivity.kt
+++ app/src/main/java/com/angeluz/freyja/MainActivity.kt
@@ -4,25 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import com.angeluz.freyja.ApiViewModel
-import com.angeluz.freyja.screens.PostsScreen
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MaterialTheme {
-                Home()
-            }
+            MaterialTheme { Surface { Text("Freyja viva ✨") } }
         }
     }
-}
-
-@Composable
-fun Home() {
-    // Muestra mensaje y la lista de ejemplo
-    Text("Hola Freyja ✨")
-    PostsScreen(vm = ApiViewModel())
 }
